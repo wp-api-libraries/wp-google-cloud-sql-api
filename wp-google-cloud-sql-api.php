503e4c155c53b28408151b1380576e5d65a395fe
Brandon Hubbard <bkhubbard@gmail.com>
2017-10-16 20:25:09 -0700

    started library

---
+++ wp-google-cloud-sql-api.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * WP Google Cloud SQL API
+ *
+ * @link https://cloud.google.com/sql/docs/mysql/admin-api/v1beta4/
+ * @package WP-API-Libraries\WP-Google-Cloud-SQL-API
+ */
+/*
+* Plugin Name: WP Google Cloud SQL API
+* Plugin URI: https://github.com/wp-api-libraries/wp-google-cloud-sql-api
+* Description: Perform API requests to Google Cloud SQL in WordPress.
+* Author: WP API Libraries
+* Version: 1.0.0
+* Author URI: https://wp-api-libraries.com
+* GitHub Plugin URI: https://github.com/wp-api-libraries/wp-google-cloud-sql-api
+* GitHub Branch: master
+*/
+/* Exit if accessed directly. */
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+/* Check if class exists. */
+if ( ! class_exists( 'GoogleCloudSQLAPI' ) ) {
+	
+	/**
+	 * GoogleCloudSQLAPI Class.
+	 */
+	class GoogleCloudSQLAPI {
+		
+		
+		/* BACKUP RUNS. */
+		
+		
+		/* DATABASES. */
+		
+		
+		/* FLAGS. */
+		
+		/* INSTANCES. */
+		
+		/* OPERATIONS. */
+		
+		/* SSL CERTS. */
+		
+		/* TIERS. */
+		
+		/* USERS. */
+		
+
+
+	} // End Class;
+}
